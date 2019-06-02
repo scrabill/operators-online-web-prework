b6115f78381b35a55be652ef1e4364e1a00a56b0
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-02 23:25:00 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -10,7 +10,7 @@ end
 
 def not_safe?(speed)
 
-  speed < 40 || speed > 60 ? return true : return false
+  speed < 40 || speed > 60 ? true : false
 
     conditional ? action_if_true : action_if_false
 
